f54361a56ad776030589949e9b64b4cd09fa9e63
SeokHui Jo <suchie777@gmail.com>
2025-03-04 17:18:11 +0900

    유니티 끄기전까지 파라미터값 툴에 저장되도록 수정

--- Assets/Scripts/Editor/AutoObjectGenerator.cs
+++ Assets/Scripts/Editor/AutoObjectGenerator.cs
@@ -17,10 +17,15 @@ public class AutoObjectGenerator : ScriptableWizard
         }
     }
     public Terrain gameTerrain = null;
-    public Texture2D mapTexture = null;
+    public Texture2D gameTexture = null;
     public Color color = Color.red;
     public GameObject gameObject = null;
     private List<Pos> _positionList = new();
+
+    private static Terrain saveTerrain;
+    private static Texture2D saveTexture;
+    private static Color saveColor;
+    private static GameObject saveObject;
     [MenuItem(("Tools/AutoObjectGenerator"))]
 
     public new static void Show()
@@ -28,27 +33,56 @@ public class AutoObjectGenerator : ScriptableWizard
         DisplayWizard<AutoObjectGenerator>("오브젝트 생성");
     }
 
+    private void OnEnable()
+    {
+        if(saveTerrain !=null)
+            gameTerrain = saveTerrain;
+
+        if(saveTerrain != null)
+            gameTexture = saveTexture;
+
+        if(saveObject != null)
+            gameObject = saveObject;
+
+        if(saveColor != new Color(0,0,0,0))
+            color = saveColor;
+    }
+
     private void OnWizardUpdate()
     {
-        if (gameTerrain == null || gameTerrain == null || mapTexture == null)
+        if (gameTerrain == null || gameTerrain == null || gameTexture == null)
         {
             isValid = false;
         }
         else
+        {
             isValid = true;
+        }
+
+        if(saveTerrain != gameTerrain)
+            saveTerrain = gameTerrain;
+
+        if(saveTexture != gameTexture)
+            saveTexture = gameTexture;
+
+        if(saveObject != gameObject)
+            saveObject = gameObject;
+
+        if(saveColor != color)
+            saveColor = color;
     }
 
     private void OnWizardCreate()
     {
         _positionList = new();
-        float widthRatio = gameTerrain.terrainData.size.x / mapTexture.width;
-        float heightRatio = gameTerrain.terrainData.size.z / mapTexture.height;
+        float widthRatio = gameTerrain.terrainData.size.x / gameTexture.width;
+        float heightRatio = gameTerrain.terrainData.size.z / gameTexture.height;
 
-        for (int y = 0; y < mapTexture.height; ++y)
+        for (int y = 0; y < gameTexture.height; ++y)
         {
-            for (int x = 0; x < mapTexture.width; ++x)
+            for (int x = 0; x < gameTexture.width; ++x)
             {
-                Color pixel = mapTexture.GetPixel(x, y);
+                Color pixel = gameTexture.GetPixel(x, y);
                 pixel.a = color.a;
 
                 if (pixel == color)
@@ -64,7 +98,7 @@ public class AutoObjectGenerator : ScriptableWizard
         {
 
             Vector3 terrainLocalPos = gameTerrain.transform.position + new Vector3(pos.X * widthRatio, 600f, pos.Y * heightRatio);
-            Ray ray = new Ray(terrainLocalPos, Vector3.down);
+            Ray ray = new(terrainLocalPos, Vector3.down);
 
             if (Physics.Raycast(ray, out RaycastHit rayhit, 1000f, layerMask))
             {
